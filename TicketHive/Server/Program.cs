852ecf5e9dffc6708520cecf9a31c2be30c941fb
Jontzey <Jonathan.andersson1997@outlook.com>
2023-04-12 19:15:28 +0200

    Cookies

--- TicketHive/Server/Program.cs
+++ TicketHive/Server/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -7,7 +8,9 @@ using TicketHive.Server.Enums;
 using TicketHive.Server.Models;
 using TicketHive.Server.Repositories;
 using TicketHive.Server.Repository;
-
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -33,7 +36,7 @@ builder.Services.AddIdentityServer()
 	});
 
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) }); builder.Services.AddBlazoredLocalStorage();
+
 builder.Services.AddScoped<IEventRepository, EventRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 
