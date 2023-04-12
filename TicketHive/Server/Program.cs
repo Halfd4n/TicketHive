fe7c80523f2bcbdbc6c86deda2064defdfe9e501
j10h8 <john.hallbeck@outlook.com>
2023-04-12 15:04:13 +0200

    Minor fixes before merge

--- TicketHive/Server/Program.cs
+++ TicketHive/Server/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using TicketHive.Server.Data;
+using TicketHive.Server.Enums;
 using TicketHive.Server.Models;
 using TicketHive.Server.Repositories;
 using TicketHive.Server.Repository;
@@ -53,86 +54,86 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
 // Create an ApplicationUser with Admin role and a UserModel user
-//using (var serviceProvider = builder.Services.BuildServiceProvider())
-//{
-//	// Create instances from DI container 
-//	var applicationDbContext = serviceProvider.GetRequiredService<ApplicationDbContext>();
-//	var mainDbContext = serviceProvider.GetRequiredService<MainDbContext>();
-//	var signInManager = serviceProvider.GetRequiredService<SignInManager<ApplicationUser>>();
-//	var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+using (var serviceProvider = builder.Services.BuildServiceProvider())
+{
+	// Create instances from DI container 
+	var applicationDbContext = serviceProvider.GetRequiredService<ApplicationDbContext>();
+	var mainDbContext = serviceProvider.GetRequiredService<MainDbContext>();
+	var signInManager = serviceProvider.GetRequiredService<SignInManager<ApplicationUser>>();
+	var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
 
-//	// Create database if it doesn't already exist 
-//	applicationDbContext.Database.Migrate();
+	// Create database if it doesn't already exist 
+	applicationDbContext.Database.Migrate();
 
-//	// Create role "Admin" if it doesn't exist in the database  
-//	// Check if role exists in database 
-//	if (!roleManager.RoleExistsAsync("Admin").GetAwaiter().GetResult())
-//	{
-//		// If role "Admin" doesn't exist, create new instance of IdentityRole with name "Admin" 
-//		IdentityRole adminRole = new()
-//		{
-//			Name = "Admin"
-//		};
+	// Create role "Admin" if it doesn't exist in the database  
+	// Check if role exists in database 
+	if (!roleManager.RoleExistsAsync("Admin").GetAwaiter().GetResult())
+	{
+		// If role "Admin" doesn't exist, create new instance of IdentityRole with name "Admin" 
+		IdentityRole adminRole = new()
+		{
+			Name = "Admin"
+		};
 
-//		// ...and add to database 
-//		roleManager.CreateAsync(adminRole).GetAwaiter().GetResult();
-//	}
+		// ...and add to database 
+		roleManager.CreateAsync(adminRole).GetAwaiter().GetResult();
+	}
 
-//	// Create an admin user if it doesn't already exist one in the database 
-//	// Check if admin user exists
-//	if (signInManager.UserManager.FindByNameAsync("admin").GetAwaiter().GetResult() == null)
-//	{
-//		// If admin user doesn't exist, create new ApplicationUser with name "admin"...
-//		ApplicationUser admin = new()
-//		{
-//			UserName = "admin",
-//			Country = Country.Sweden
-//		};
+	// Create an admin user if it doesn't already exist one in the database 
+	// Check if admin user exists
+	if (signInManager.UserManager.FindByNameAsync("admin").GetAwaiter().GetResult() == null)
+	{
+		// If admin user doesn't exist, create new ApplicationUser with name "admin"...
+		ApplicationUser admin = new()
+		{
+			UserName = "admin",
+			Country = Country.Sweden
+		};
 
-//		// ...add to IdentityDb... 
-//		signInManager.UserManager.CreateAsync(admin, "Password1234!").GetAwaiter().GetResult();
+		// ...add to IdentityDb... 
+		signInManager.UserManager.CreateAsync(admin, "Password1234!").GetAwaiter().GetResult();
 
-//		// ...create new user based on admin user and add to MainDb...
-//		var mainUser1 = signInManager.UserManager.FindByNameAsync("admin").GetAwaiter().GetResult();
-//		mainDbContext.Users.Add(new()
-//		{
-//			Id = mainUser1.Id,
-//			Username = mainUser1.UserName!,
-//			Country = mainUser1.Country
-//		});
+		// ...create new user based on admin user and add to MainDb...
+		var mainUser1 = signInManager.UserManager.FindByNameAsync("admin").GetAwaiter().GetResult();
+		mainDbContext.Users.Add(new()
+		{
+			Id = mainUser1.Id,
+			Username = mainUser1.UserName!,
+			Country = mainUser1.Country
+		});
 
-//		await mainDbContext.SaveChangesAsync();
+		await mainDbContext.SaveChangesAsync();
 
-//		// ...and add the admin user to the "Admin" role in Identity database 
-//		signInManager.UserManager.AddToRoleAsync(admin, "Admin").GetAwaiter().GetResult();
-//	}
+		// ...and add the admin user to the "Admin" role in Identity database 
+		signInManager.UserManager.AddToRoleAsync(admin, "Admin").GetAwaiter().GetResult();
+	}
 
-//	// Create a regular user if it doesn't already exist one in the database
-//	// Check if regular user exists
-//	if (signInManager.UserManager.FindByNameAsync("user").GetAwaiter().GetResult() == null)
-//	{
-//		// If regular user doesn't exist, create new ApplicationUser with name "user"...
-//		ApplicationUser user = new()
-//		{
-//			UserName = "user",
-//			Country = Country.Sweden
-//		};
+	// Create a regular user if it doesn't already exist one in the database
+	// Check if regular user exists
+	if (signInManager.UserManager.FindByNameAsync("user").GetAwaiter().GetResult() == null)
+	{
+		// If regular user doesn't exist, create new ApplicationUser with name "user"...
+		ApplicationUser user = new()
+		{
+			UserName = "user",
+			Country = Country.Sweden
+		};
 
-//		// ...and add to IdentityDb 
-//		signInManager.UserManager.CreateAsync(user, "Password1234!").GetAwaiter().GetResult();
+		// ...and add to IdentityDb 
+		signInManager.UserManager.CreateAsync(user, "Password1234!").GetAwaiter().GetResult();
 
-//		// ...create new user based on user and add to MainDb...
-//		var mainUser2 = signInManager.UserManager.FindByNameAsync("user").GetAwaiter().GetResult();
-//		mainDbContext.Users.Add(new()
-//		{
-//			Id = mainUser2.Id,
-//			Username = mainUser2.UserName!,
-//			Country = mainUser2.Country
-//		});
+		// ...create new user based on user and add to MainDb...
+		var mainUser2 = signInManager.UserManager.FindByNameAsync("user").GetAwaiter().GetResult();
+		mainDbContext.Users.Add(new()
+		{
+			Id = mainUser2.Id,
+			Username = mainUser2.UserName!,
+			Country = mainUser2.Country
+		});
 
-//		await mainDbContext.SaveChangesAsync();
-//	}
-//}
+		await mainDbContext.SaveChangesAsync();
+	}
+}
 
 var app = builder.Build();
 
