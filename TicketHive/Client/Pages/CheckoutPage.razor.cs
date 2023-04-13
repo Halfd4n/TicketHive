2800992f2e86f60d2b584a7dbbabeca7899f24b4
Benjamin Bonnerup <112319740+Halfd4n@users.noreply.github.com>
2023-04-13 20:09:39 +0200

    Booking done

---
+++ TicketHive/Client/Pages/CheckoutPage.razor.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
+using Microsoft.JSInterop;
+using TicketHive.Client;
+using TicketHive.Client.Shared;
+using TicketHive.Shared.Models;
+using Newtonsoft.Json;
+
+namespace TicketHive.Client.Pages;
+
+public partial class CheckoutPage
+{
+    public List<EventModel> MyTickets { get; set; } = new();
+    public List<EventModel>? AllEvents { get; set; } = new();
+    public decimal TotalCost { get; set; }
+    public UserModel? SignedInUser { get; set; } = new();
+    
+    protected async override Task OnInitializedAsync()
+    {
+        AllEvents = await _eventService.GetEventsAsync();
+
+        if(AllEvents != null)
+        {
+            await CheckShoppingCartContent();
+        }
+
+        CalculateTotalCost();
+
+        var authenticationState = await _authentication.GetAuthenticationStateAsync();
+
+        var userId = authenticationState.User.Claims.FirstOrDefault(c => c.Type == "sub")?.Value;
+
+        SignedInUser = await _userService.GetUserByIdAsync(userId);
+
+    }
+
+    private async Task CheckShoppingCartContent()
+    {
+        foreach (EventModel eventModel in AllEvents!)
+        {
+            string jsonCart = await _storage.GetItemAsStringAsync(eventModel.Id.ToString());
+
+            if (!String.IsNullOrWhiteSpace(jsonCart))
+            {
+                EventModel eventFromShoppingCart = JsonConvert.DeserializeObject<EventModel>(jsonCart);
+
+                MyTickets.Add(eventFromShoppingCart);
+            }
+        }
+    }
+
+    private async Task ConfirmPurchase()
+    {
+        foreach(EventModel eventModel in MyTickets)
+        {
+            await _eventService.BookEventAsync(SignedInUser.Id, eventModel.Id, eventModel.NumberOfTickets);
+        }
+
+        await _storage.ClearAsync();
+
+        _navigation.NavigateTo("/confirmed");
+    }
+
+    private void CalculateTotalCost()
+    {
+        foreach(EventModel eventModel in MyTickets)
+        {
+            TotalCost += (eventModel.Price * eventModel.NumberOfTickets);
+        }
+    }
+
+    private void NavigateToEvents()
+    {
+        _navigation.NavigateTo("/allevents");
+    }
+
+    private void ReturnToShoppingCart()
+    {
+        _navigation.NavigateTo("/cart");
+    }
+}
--- TicketHive/Client/Pages/ShowCart.razor.cs
+++ TicketHive/Client/Pages/ShowCart.razor.cs
@@ -1,22 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components;
-using System.Net.Http;
-using System.Net.Http.Json;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.Forms;
-using Microsoft.AspNetCore.Components.Routing;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.Web.Virtualization;
-using Microsoft.AspNetCore.Components.WebAssembly.Http;
-using Microsoft.JSInterop;
-using TicketHive.Client;
-using TicketHive.Client.Shared;
 using TicketHive.Shared.Models;
 using Newtonsoft.Json;
-using System.ComponentModel;
 
 namespace TicketHive.Client.Pages
 {
@@ -109,6 +92,16 @@ namespace TicketHive.Client.Pages
             await CheckShoppingCartContent();
 
             StateHasChanged();
+        }
+
+        private void PurchaseTickets()
+        {
+            _navigation.NavigateTo("/checkout");
+        }
+
+        private void NavigateToHome()
+        {
+            _navigation.NavigateTo("");
         }
     }
 }
